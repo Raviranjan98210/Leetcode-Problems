9d0b31234beb0e3318d455089f72e8e67360a4a6
RAVI RANJAN KUMAR <32854109+Raviranjan98210@users.noreply.github.com>
2024-08-05 22:41:30 +0530

    Time: 52 ms (92.07%) | Memory: 50.9 MB (95.13%) - LeetSync

--- 485-max-consecutive-ones/max-consecutive-ones.js
+++ 485-max-consecutive-ones/max-consecutive-ones.js
@@ -3,27 +3,43 @@
  * @return {number}
  */
 var findMaxConsecutiveOnes = function(nums) {
-    let max = 0; 
-    let i=0,j=1;
-[1,1]
-    if(nums.length === 1) return nums[0]
-    while(j < nums.length) {
-        if(nums[i] === 1) {
-            while(nums[j] === 1) {
-                j++;
-            }
-            max = Math.max(max,j-i)
-             i = j;
-             j++;
+    // let max = 0; 
+    // let i=0,j=1;
+    // // Approach -1 
+    // if(nums.length === 1) return nums[0]
+    // while(j < nums.length) {
+    //     if(nums[i] === 1) {
+    //         while(nums[j] === 1) {
+    //             j++;
+    //         }
+    //         max = Math.max(max,j-i)
+    //          i = j;
+    //          j++;
 
-        }else {
-            i++;
-            j++;
-            if(j>= nums.length) {
-                return Math.max(max,nums[i])
-            }
+    //     }else {
+    //         i++;
+    //         j++;
+    //         if(j>= nums.length) {
+    //             return Math.max(max,nums[i])
+    //         }
+    //     }
+
+    // }
+    // return max;
+
+    // Approach- 2
+
+    let count= 0;
+    let maxCount = 0;
+    for(let i=0; i<nums.length; i++){
+        if(nums[i]===1) {
+            count++;
+            maxCount = Math.max(maxCount,count)
         }
-
+        else {
+            count = 0;
+        }
     }
-    return max;
+    return maxCount;
+
 };
