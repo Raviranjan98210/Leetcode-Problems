b9519baf6ef73a22677794d955ac98840fb2e466
RAVI RANJAN KUMAR <32854109+Raviranjan98210@users.noreply.github.com>
2024-08-09 01:53:29 +0530

    Time: 53 ms (88.62%) | Memory: 51.5 MB (61.28%) - LeetSync

--- 169-majority-element/majority-element.js
+++ 169-majority-element/majority-element.js
@@ -3,27 +3,46 @@
  * @return {number}
  */
 var majorityElement = function(nums) {
-    // take hasmap 
-    const countMap = new Map();
+    // -------- Approach-1 ------------
+    // step: 1 take hasmap 
+    //check if map has current element if yes then increase the count 
+    // else insert the number with count 1
+    //
+//     const countMap = new Map();
 
-   for(let i=0;i<nums.length; i++) {
-        //check if map has current element if yes then increase the number 
-        // else insert the number with count 1
+//    for(let i=0;i<nums.length; i++) {
+//         if(countMap.has(nums[i])) {
+//             let count = countMap.get(nums[i]) + 1 
+//             countMap.set(nums[i],count)
+//         }else {
+//             countMap.set(nums[i],1)
+//         }
+//     }
+    
+//     for([key,value] of countMap) {
+//        if(value > nums.length/2) {
+//            return key;
+//        }
+//     }
 
-        if(countMap.has(nums[i])) {
-            let count = countMap.get(nums[i]) + 1 
-            countMap.set(nums[i],count)
+//     return elm;
+
+    // Moore’s Voting Algorithm
+    let count = 0;
+    let elm;
+    const n = nums.length
+
+    for(let i=0;i<n; i++) {
+        if(count === 0 ) {
+            elm=nums[i]
+            count++;
+            
+        }else if(nums[i]=== elm){
+            count++;
         }else {
-            countMap.set(nums[i],1)
+            count--;
         }
     }
-    
-    for([key,value] of countMap) {
-       if(value > nums.length/2) {
-           return key;
-       }
-    }
-
     return elm;
     
 };
