23934b9d430589f12cd9ad2c08b9cd1ecbfbdcf3
RAVI RANJAN KUMAR <32854109+Raviranjan98210@users.noreply.github.com>
2024-08-17 01:37:27 +0530

    Time: 71 ms (5.16%) | Memory: 52.1 MB (5.04%) - LeetSync

---
+++ 54-spiral-matrix/spiral-matrix.js
@@ -0,0 +1,50 @@
+/**
+ * @param {number[][]} matrix
+ * @return {number[]}
+ */
+var spiralOrder = function(matrix) {
+    // right->bottom-> left->top
+    let n = matrix.length;
+    let m = matrix[0].length;
+    let left =0;
+    let top = 0;
+    let right = m-1
+    let bottom = n-1;
+   
+   let spiral=[]
+   while(left <= right && top <= bottom) {
+    // From left to right (i.e left constant here)
+        for(let i=top; i<=right; i++) {
+            spiral.push(matrix[left][i])
+        }
+        console.log(spiral)
+        top++;
+    // From top to bottom (i.e right constant here)
+        for(let i=top; i<=bottom; i++) {
+            spiral.push(matrix[i][right])
+        }
+        console.log(spiral)
+        right--;
+
+    // From Right to left (i.e bottom constant here)
+    if(top<=bottom) {
+        for(let i= right; i>=left; i--) {
+            spiral.push(matrix[bottom][i]) //[2][1]
+        }
+        console.log(spiral)
+        bottom--;
+    }
+       
+
+    // From bottom to top
+    if(left<=right) {
+        for(let i=bottom; i>= top; i--) {
+            spiral.push(matrix[i][left]) //1
+        }
+        console.log(spiral)
+        left++;
+    }
+       
+     }
+    return spiral;
+};
