31186e21efc6b7b099a51c3958b17054889a8fe8
RAVI RANJAN KUMAR <32854109+Raviranjan98210@users.noreply.github.com>
2024-08-29 01:18:05 +0530

    Time: 85 ms (61.00%) | Memory: 52.3 MB (96.81%) - LeetSync

--- 18-4sum/4sum.js
+++ 18-4sum/4sum.js
@@ -4,44 +4,56 @@
  * @return {number[][]}
  */
 var fourSum = function(nums, target) {
- // Sort the array to help avoid duplicates
-    nums.sort((a, b) => a - b);
-    
-    const result = [];
-    const n = nums.length;
-    
-    for (let i = 0; i < n - 3; i++) {
-        // Skip duplicates for the first number
-        if (i > 0 && nums[i] === nums[i - 1]) continue;
+   const n = nums.length;
+//    const set = new Set()
+//     //  Brute force approach
+//     for(let i=0;i<n;i++) {
+//         for(let j=i+1;j<n;j++) {
+//             for(let k=j+1;k<n;k++){
+//                 for(let l=k+1;l<n;l++){
+//                     if(nums[i]+nums[j]+nums[k]+nums[l] === target){
+//                         const temp = [nums[i],nums[j],nums[k],nums[l]]
+//                         temp.sort((a,b)=>a-b)
+//                         set.add(JSON.stringify(temp))
+//                     }
+//                 }
+//             }
+//         }
+//     }
 
-        for (let j = i + 1; j < n - 2; j++) {
-            // Skip duplicates for the second number
-            if (j > i + 1 && nums[j] === nums[j - 1]) continue;
+//   let ans = []
+//   for(let item of set){
+//     ans.push(JSON.parse(item))
+//   }
+//   return ans;
 
-            let left = j + 1;
-            let right = n - 1;
-            
-            while (left < right) {
-                const sum = nums[i] + nums[j] + nums[left] + nums[right];
-                
-                if (sum === target) {
-                    result.push([nums[i], nums[j], nums[left], nums[right]]);
-                    
-                    // Skip duplicates for the third number
-                    while (left < right && nums[left] === nums[left + 1]) left++;
-                    // Skip duplicates for the fourth number
-                    while (left < right && nums[right] === nums[right - 1]) right--;
-
-                    left++;
-                    right--;
-                } else if (sum < target) {
-                    left++;
-                } else {
-                    right--;
-                }
+// Optimal solution 
+// sort the array
+// Now check if the 
+nums.sort((a,b)=> a-b)
+const res = []
+for(let i=0;i<n;i++){
+    if(i>0 && nums[i] === nums[i-1]) continue;
+    for(let j=i+1;j<n;j++){
+        if(j> i+1 && nums[j] === nums[j-1]) continue;
+        let k = j+1;
+        let l = n-1;
+        while(k<l){
+            let sum = nums[i]+nums[j]+nums[k]+nums[l]
+            if(sum<target ){
+                k++;
+            }else if(sum>target ){
+                l--;
+            }else{
+                const temp = [nums[i],nums[j],nums[k],nums[l]]
+                res.push(temp)
+                k++;
+                l--;
+                while(k<l && nums[k]=== nums[k-1]) k++;
+                while(k<l && nums[l] === nums[l+1]) l--;
             }
         }
     }
-    
-    return result;
+}
+return res;
 };
