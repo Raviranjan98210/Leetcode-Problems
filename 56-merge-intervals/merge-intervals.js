4404fe361e0896c18c62477592a40112863bc983
RAVI RANJAN KUMAR <32854109+Raviranjan98210@users.noreply.github.com>
2024-09-01 20:37:11 +0530

    Time: 230 ms (5.01%) | Memory: 59.7 MB (34.90%) - LeetSync

---
+++ 56-merge-intervals/merge-intervals.js
@@ -0,0 +1,30 @@
+/**
+ * @param {number[][]} intervals
+ * @return {number[][]}
+ */
+var merge = function(intervals) {
+   const res = [];
+   const n = intervals.length;
+    intervals.sort((a,b)=> a[0]-b[0]);
+   
+    for(let i=0; i<n; i++) {
+        let first = intervals[i][0]
+        let end = intervals[i][1]
+
+         // If last end is within the interval of last interval then skip this as it 
+         // will also be there is that interval
+        if(res.length && res[res.length-1][1] >= end){
+            continue;
+        }
+
+        // If element is not in the interval the expand the interval else create new interval
+        for(let j = i+1; j < n; j++) {
+            if(end >= intervals[j][0]){
+                end = Math.max(end,intervals[j][1])
+            }
+        }
+
+        res.push([first,end])
+    }
+    return res;
+};
