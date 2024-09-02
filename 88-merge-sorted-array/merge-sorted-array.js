e93a2a53374dfe01e7b386bfe87b02b97d93bf7c
RAVI RANJAN KUMAR <32854109+Raviranjan98210@users.noreply.github.com>
2024-09-03 00:16:57 +0530

    Time: 60 ms (20.54%) | Memory: 52.5 MB (5.19%) - LeetSync

---
+++ 88-merge-sorted-array/merge-sorted-array.js
@@ -0,0 +1,59 @@
+/**
+ * @param {number[]} nums1
+ * @param {number} m
+ * @param {number[]} nums2
+ * @param {number} n
+ * @return {void} Do not return anything, modify nums1 in-place instead.
+ */
+var merge = function(nums1, m, nums2, n) {
+    //Brute force using extra array
+
+    // const temp = []
+    // let i = 0;
+    // let j = 0;
+    // while( i < m && j <n) {
+    //     if(nums1[i]<=nums2[j]) {
+    //         temp.push(nums1[i]);
+    //         i++;
+    //     }else {
+    //         temp.push(nums2[j]);
+    //         j++;
+    //     }
+    // }
+    // while(i<m){
+    //     temp.push(nums1[i]);
+    //     i++;
+    // }
+    // while(j<n) {
+    //     temp.push(nums2[j])
+    //     j++;
+    // }
+    
+    // for(let i=0; i< m+n; i++) {
+    //     nums1[i] = temp[i]
+    // }
+   
+    //Optimal-1 
+    let i=m-1;
+    let j = 0;
+    if(n>0){
+        nums1.splice(-n)
+    }
+
+    while( i >= 0 && j<n) {
+        if(nums1[i]>nums2[j]) {
+           [nums1[i],nums2[j]] = [nums2[j],nums1[i]]
+           i--,j++
+        }else {
+            break;
+        }
+    }
+
+    nums1.sort((a,b)=>a-b)
+    nums2.sort((a,b)=>a-b)
+    console.log(nums1,nums2)
+    for(let i=0;i<n; i++) {
+        nums1.push(nums2[i])
+    }
+   
+};
