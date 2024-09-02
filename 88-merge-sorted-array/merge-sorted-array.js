8f4d4a0b464425aad21fdda3590cb23c6e640efa
RAVI RANJAN KUMAR <32854109+Raviranjan98210@users.noreply.github.com>
2024-09-03 01:34:44 +0530

    Time: 65 ms (7.95%) | Memory: 52 MB (5.15%) - LeetSync

--- 88-merge-sorted-array/merge-sorted-array.js
+++ 88-merge-sorted-array/merge-sorted-array.js
@@ -5,7 +5,13 @@
  * @param {number} n
  * @return {void} Do not return anything, modify nums1 in-place instead.
  */
-var merge = function(nums1, m, nums2, n) {
+
+var swapIfGreater = function (arr1, arr2, ind1, ind2) {
+  if (arr1[ind1] > arr2[ind2]) {
+    [arr1[ind1], arr2[ind2]] = [arr2[ind2], arr1[ind1]];
+  }
+}
+var merge = function (nums1, m, nums2, n) {
     //Brute force using extra array
 
     // const temp = []
@@ -28,11 +34,11 @@ var merge = function(nums1, m, nums2, n) {
     //     temp.push(nums2[j])
     //     j++;
     // }
-    
+
     // for(let i=0; i< m+n; i++) {
     //     nums1[i] = temp[i]
     // }
-   
+
     //Optimal-1 
     // let i=m-1;
     // let j = 0;
@@ -55,9 +61,38 @@ var merge = function(nums1, m, nums2, n) {
     // for(let i=0;i<n; i++) {
     //     nums1.push(nums2[i])
     // }
-//    Optimal 2 
-     for(let i = m; i < nums1.length; i ++){
-        nums1[i] = nums2.shift();
+    //    Optimal 2 
+    //  for(let i = m; i < nums1.length; i ++){
+    //     nums1[i] = nums2.shift();
+    // }
+    // nums1.sort((a,b) => a - b);
+
+
+    // Optimal using gap method  (shell sort)
+    let len = m + n;
+    let gap = Math.ceil(len / 2)
+    if (n > 0) {
+        nums1.splice(-n)
     }
-    nums1.sort((a,b) => a - b);
+    while (gap > 0) {
+        let left = 0;
+        let right = left + gap;
+        while (right < len) {
+            if (left < m && right >= m) {
+                swapIfGreater(nums1, nums2, left, right - m)
+            } else if (left >= m) {
+                swapIfGreater(nums2, nums2, left - m, right - m)
+            } else {
+                swapIfGreater(nums1, nums1, left, right)
+            }
+
+            left++;
+            right++;
+        }
+        if (gap === 1) break
+        gap = Math.ceil(gap / 2)
+    }
+   for (let i = 0; i < n; i++) {
+        nums1[m + i] = nums2[i];
+    }
 };
