4606d9a747fc8d8b4b663c44ee6f06aa79b601e8
RAVI RANJAN KUMAR <32854109+Raviranjan98210@users.noreply.github.com>
2024-09-03 00:18:34 +0530

    Time: 57 ms (34.01%) | Memory: 48.9 MB (64.01%) - LeetSync

--- 88-merge-sorted-array/merge-sorted-array.js
+++ 88-merge-sorted-array/merge-sorted-array.js
@@ -34,26 +34,30 @@ var merge = function(nums1, m, nums2, n) {
     // }
    
     //Optimal-1 
-    let i=m-1;
-    let j = 0;
-    if(n>0){
-        nums1.splice(-n)
-    }
+    // let i=m-1;
+    // let j = 0;
+    // if(n>0){
+    //     nums1.splice(-n)
+    // }
 
-    while( i >= 0 && j<n) {
-        if(nums1[i]>nums2[j]) {
-           [nums1[i],nums2[j]] = [nums2[j],nums1[i]]
-           i--,j++
-        }else {
-            break;
-        }
-    }
+    // while( i >= 0 && j<n) {
+    //     if(nums1[i]>nums2[j]) {
+    //        [nums1[i],nums2[j]] = [nums2[j],nums1[i]]
+    //        i--,j++
+    //     }else {
+    //         break;
+    //     }
+    // }
 
-    nums1.sort((a,b)=>a-b)
-    nums2.sort((a,b)=>a-b)
-    console.log(nums1,nums2)
-    for(let i=0;i<n; i++) {
-        nums1.push(nums2[i])
+    // nums1.sort((a,b)=>a-b)
+    // nums2.sort((a,b)=>a-b)
+    // console.log(nums1,nums2)
+    // for(let i=0;i<n; i++) {
+    //     nums1.push(nums2[i])
+    // }
+//    Optimal 2 
+     for(let i = m; i < nums1.length; i ++){
+        nums1[i] = nums2.shift();
     }
-   
+    nums1.sort((a,b) => a - b);
 };
