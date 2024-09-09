8762c522208a353baaa49605eedb5ac302ca7c73
RAVI RANJAN KUMAR <32854109+Raviranjan98210@users.noreply.github.com>
2024-09-09 16:22:04 +0530

    Time: 80 ms (63.55%) | Memory: 61.1 MB (15.62%) - LeetSync

--- 189-rotate-array/rotate-array.js
+++ 189-rotate-array/rotate-array.js
@@ -10,22 +10,23 @@ var rotate = function(nums, k) {
     if(k>0) {
 
         // Method -> 1
-        // const temp = []
-        // for(let i= n-k; i<n; i++) {
-        //     temp.push(nums[i])
-        // }
+        const temp = []
+        for(let i= n-k; i<n; i++) {
+            temp.push(nums[i])
+        }
+    //  temp = [6,1,2]
+        for(let i= 0;i<=n-k;i++) {
+            temp[i+k] = nums[i]
+        }
 
-        // for(let i= n-k-1;i>=0;i--) {
-        //     nums[i+k] = nums[i]
-        // }
-
-        // for(let i=0;i<k;i++) {
-        //     nums[i]= temp[i]
-        // }
+        for(let i=0;i<n;i++) {
+            nums[i]= temp[i]
+        }
+        
 
         // Method -2
 
-        nums.unshift(...nums.splice(-k))
+        // nums.unshift(...nums.splice(-k))
 
         // Method -3 (use reverse method)
     }
