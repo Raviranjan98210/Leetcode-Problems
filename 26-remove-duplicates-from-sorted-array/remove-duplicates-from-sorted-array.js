41bde05e8c9b8c57eba64279bebdab48c97c71d6
RAVI RANJAN KUMAR <32854109+Raviranjan98210@users.noreply.github.com>
2025-09-12 17:44:28 +0530

    Time: 0 ms (100.00%) | Memory: 58.6 MB (10.25%) - LeetSync

--- 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.js
+++ 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.js
@@ -3,23 +3,33 @@
  * @return {number}
  */
 var removeDuplicates = function(nums) {
-    const l = nums.length;
-    let j = 1;
-    let i = 0; 
-    const arr = []
-    while(i < l ) {
-        if(nums[i]=== nums[j]) {
-            j++;
-        }else {
-          arr.push(nums[i])
-          i=j;
-          j++;
+    // const l = nums.length;
+    // let j = 1;
+    // let i = 0; 
+    // const arr = []
+    // while(i < l ) {
+    //     if(nums[i]=== nums[j]) {
+    //         j++;
+    //     }else {
+    //       arr.push(nums[i])
+    //       i=j;
+    //       j++;
+    //     }
+    // }
+
+    // for(let i=0; i<arr.length ; i++) {
+    //         nums[i]= arr[i]
+    // }
+    // nums.splice(arr.length)
+
+    ///Approach 2 (optimal)
+
+    let u = 0;
+    for(let i=0; i < nums.length; i++){
+        if(nums[i]>nums[u]) {
+            u = u+1;
+            nums[u] = nums[i]
         }
     }
-
-    for(let i=0; i<arr.length ; i++) {
-            nums[i]= arr[i]
-    }
-    nums.splice(arr.length)
-
+    return u+1;
 };
