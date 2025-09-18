5f3aeb34695a0562ef8da3ffa3724e667fc97c40
RAVI RANJAN KUMAR <32854109+Raviranjan98210@users.noreply.github.com>
2025-09-19 01:47:51 +0530

    Time: 3 ms (38.28%) | Memory: 61.1 MB (10.41%) - LeetSync

--- 283-move-zeroes/move-zeroes.js
+++ 283-move-zeroes/move-zeroes.js
@@ -4,16 +4,37 @@
  */
 var moveZeroes = function(nums) {
     // Brute Force
-    const nonZero = [];
-    for(let i = 0; i<nums.length;i++){
-        if(nums[i] !== 0) {
-            nonZero.push(nums[i])
+    // const nonZero = [];
+    // for(let i = 0; i<nums.length;i++){
+    //     if(nums[i] !== 0) {
+    //         nonZero.push(nums[i])
+    //     }
+    // }
+    // for(let i=0; i < nonZero.length; i++){
+    //     nums[i] = nonZero[i]
+    // }
+    // for(let i = nonZero.length; i<nums.length; i++){
+    //     nums[i] = 0
+    // }
+
+    let p1=0;
+    let p2=1;
+
+   while(p2<nums.length){
+        if(nums.length === 1)  return nums;
+
+        if(nums[p1] === 0 && nums[p2]!==0){
+            [nums[p1],nums[p2]] = [nums[p2],nums[p1]]
+            p1++;
+            p2++;
+        } else if(nums[p1]!== 0) {
+            p1++;
+            p2++;
+        }else {
+            p2++
         }
     }
-    for(let i=0; i < nonZero.length; i++){
-        nums[i] = nonZero[i]
-    }
-    for(let i = nonZero.length; i<nums.length; i++){
-        nums[i] = 0
-    }
+
+
+
 };
