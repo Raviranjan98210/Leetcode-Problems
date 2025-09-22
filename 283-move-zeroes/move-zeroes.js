dd224ef94dd1acef58970383c276fe36d464dbef
RAVI RANJAN KUMAR <32854109+Raviranjan98210@users.noreply.github.com>
2025-09-22 22:43:56 +0530

    Time: 0 ms (100.00%) | Memory: 59.3 MB (78.66%) - LeetSync

--- 283-move-zeroes/move-zeroes.js
+++ 283-move-zeroes/move-zeroes.js
@@ -17,24 +17,34 @@ var moveZeroes = function(nums) {
     //     nums[i] = 0
     // }
 
-    let p1=0;
-    let p2=1;
+//     let p1=0;
+//     let p2=1;
 
-   while(p2<nums.length){
-        if(nums.length === 1)  return nums;
+//    while(p2<nums.length){
+//         if(nums.length === 1)  return nums;
+//         if(nums[p1] === 0 && nums[p2]!==0){
+//             [nums[p1],nums[p2]] = [nums[p2],nums[p1]]
+//             p1++;
+//             p2++;
+//         } else if(nums[p1]!== 0) {
+//             p1++;
+//             p2++;
+//         }else {
+//             p2++
+//         }
+//     }
 
-        if(nums[p1] === 0 && nums[p2]!==0){
-            [nums[p1],nums[p2]] = [nums[p2],nums[p1]]
-            p1++;
-            p2++;
-        } else if(nums[p1]!== 0) {
-            p1++;
-            p2++;
-        }else {
-            p2++
-        }
+//Aproach: 3
+
+let p = 0;
+for(let i=0; i<nums.length; i++){
+    if(nums[i] !== 0){
+        nums[p] = nums[i];
+        p++;
     }
-
-
+}
+for(let i=p; i<nums.length;i++) {
+    nums[i] = 0
+}
 
 };
