0a6221e27b778b8c97228afabce50aa04219a0aa
RAVI RANJAN KUMAR <32854109+Raviranjan98210@users.noreply.github.com>
2025-09-24 00:11:32 +0530

    Time: 0 ms (100.00%) | Memory: 56.6 MB (42.87%) - LeetSync

--- 268-missing-number/missing-number.js
+++ 268-missing-number/missing-number.js
@@ -3,33 +3,13 @@
  * @return {number}
  */
 var missingNumber = function(nums) {
+    
+    let actual = 0;
     const n = nums.length;
+    const expected =(n*(n+1))/2
 
-    // approach -1
-    let temp ;
-
-    nums.sort((a,b)=>a-b)
-    for(let i=0;i<=n;i++) {
-        console.log(nums[i],i)
-        if(nums[i]!==i)  {
-            temp = i
-            break;
-        }
+    for(let i=0; i<nums.length; i++){
+        actual += nums[i]
     }
-
-    return temp;
-
-
-    // Approach -2 (summation of n natural number)
-//     const sum = (n*(n+1))/2
-//     let sum1=0;
-//     for(let i=0; i<n;i++){
-//         sum1 += nums[i]
-//     }
-
-//    return sum-sum1
-
-
-    //Approach - 3 (using XOR) -> do it when revising
-
+    return expected-actual;
 };
