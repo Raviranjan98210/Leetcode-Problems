94e60fc3c73e81c7548efceaffbc2afb2e5fbfee
RAVI RANJAN KUMAR <32854109+Raviranjan98210@users.noreply.github.com>
2025-09-24 23:30:31 +0530

    Time: 7 ms (42.22%) | Memory: 61.6 MB (7.39%) - LeetSync

--- 136-single-number/single-number.js
+++ 136-single-number/single-number.js
@@ -4,22 +4,22 @@
  */
 var singleNumber = function(nums) {
     //   Brute Force
-    // const cmap = new Map();
-    // for(let i=0; i < nums.length; i++) {
-    //     cmap.set(nums[i], (cmap.get(nums[i]) || 0 ) + 1  )
-    // }
-    // for(let i=0; i < nums.length; i++) {
-    //    if( cmap.get(nums[i]) === 1) {
-    //         return nums[i]
-    //    }
-    // }
+    const cmap = new Map();
+    for(let i=0; i < nums.length; i++) {
+        cmap.set(nums[i], (cmap.get(nums[i]) || 0 ) + 1  )
+    }
+    for(let [key,value] of cmap){
+        if(value === 1){
+            return key;
+        }
+    }
 
     // Optimal Solution
-    let xor = 0;
+    // let xor = 0;
 
-    for(let i=0; i < nums.length; i++) {
-       xor ^=nums[i]
-    }
-    return xor;
+    // for(let i=0; i < nums.length; i++) {
+    //    xor ^=nums[i]
+    // }
+    // return xor;
 
 };
