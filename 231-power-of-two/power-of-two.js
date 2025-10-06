71e2f8d9eab29215f447a96472d3eebf9d57f83d
RAVI RANJAN KUMAR <32854109+Raviranjan98210@users.noreply.github.com>
2025-10-07 00:21:31 +0530

    Time: 0 ms (100.00%) | Memory: 55.7 MB (84.21%) - LeetSync

--- 231-power-of-two/power-of-two.js
+++ 231-power-of-two/power-of-two.js
@@ -3,6 +3,8 @@
  * @return {boolean}
  */
 var isPowerOfTwo = function(n) {
+   /* 
+   Approach 1
     if(n===1) return true
     let p = 1;
     let _isPowerOfTwo = false
@@ -14,4 +16,15 @@ var isPowerOfTwo = function(n) {
         }
     }
     return _isPowerOfTwo;
+    */
+
+    // Approach - 2 (using recursion)
+
+    // base case
+    if(n===1) {return true}
+    else if(n<1 || n % 2 !==0){
+        return false
+    }
+    return isPowerOfTwo(n/2)
+
 };
