28ce6b920ae408b69552779b05c585a285ccbaba
RAVI RANJAN KUMAR <32854109+Raviranjan98210@users.noreply.github.com>
2025-10-07 23:59:54 +0530

    Time: 0 ms (100.00%) | Memory: 58.8 MB (37.38%) - LeetSync

--- 792-binary-search/binary-search.ts
+++ 792-binary-search/binary-search.ts
@@ -1,17 +1,33 @@
 function search(nums: number[], target: number): number {
-    let l = 0;
-    let r = nums.length-1;
+    // let l = 0;
+    // let r = nums.length-1;
 
-    while(l<=r) {
-       let mid = Math.floor((l+r)/2)
-       if(target === nums[mid]){
-        return mid;
-       }else if(target<nums[mid]) {
-            r = mid-1
-       }else {
-        l = mid+1
-       }
+    // while(l<=r) {
+    //    let mid = Math.floor((l+r)/2)
+    //    if(target === nums[mid]){
+    //     return mid;
+    //    }else if(target<nums[mid]) {
+    //         r = mid-1
+    //    }else {
+    //     l = mid+1
+    //    }
 
+    // }
+    // return -1;
+    let start = 0;
+    let end = nums.length-1
+
+    while(start <= end) {
+        let mid = Math.floor((start+end)/2);
+        const value = nums[mid]
+        if(value === target){
+            return mid
+        } 
+        if(value < target){
+            start = mid + 1
+        }else {
+            end = mid -1
+        }
     }
-    return -1;
+    return -1
 };
