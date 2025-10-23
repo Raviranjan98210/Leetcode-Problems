4896bcf672ede728c2ccd2c6d919d89d394622ac
RAVI RANJAN KUMAR <32854109+Raviranjan98210@users.noreply.github.com>
2025-10-24 00:20:15 +0530

    Time: 45 ms (98.34%) | Memory: 62.6 MB (77.20%) - LeetSync

--- 160-intersection-of-two-linked-lists/intersection-of-two-linked-lists.js
+++ 160-intersection-of-two-linked-lists/intersection-of-two-linked-lists.js
@@ -12,16 +12,33 @@
  * @return {ListNode}
  */
 var getIntersectionNode = function(headA, headB) {
-    const nodeSet = new Set();
 
-    while(headA){
-          nodeSet.add(headA);
-          headA = headA.next;
+// approach-1
+    // const nodeSet = new Set();
+
+    // while(headA){
+    //       nodeSet.add(headA);
+    //       headA = headA.next;
+    // }
+    // while(headB){
+    //      if(nodeSet.has(headB)) return headB;
+    //       headB = headB.next;
+    // }
+    // return null;
+
+// Approach-2
+    let pA = headA;
+    let pB = headB;
+
+    while(pA != pB){
+        pA = pA.next;
+        pB = pB.next;
+        
+        if(pA === pB) return pA;
+        if(!pA){ pA = headB;}
+        if(!pB) {pB = headA;}
+
+        
     }
-    while(headB){
-         if(nodeSet.has(headB)) return headB;
-          headB = headB.next;
-    }
-    return null;
-    
+    return pA;
 };
