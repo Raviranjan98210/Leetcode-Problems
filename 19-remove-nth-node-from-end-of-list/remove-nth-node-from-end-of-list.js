43eb8b8b216ed97f813dc324521b5646ec72d03f
RAVI RANJAN KUMAR <32854109+Raviranjan98210@users.noreply.github.com>
2026-06-04 22:45:18 +0530

    Time: 0 ms (100.00%) | Memory: 54.7 MB (45.29%) - LeetSync

--- 19-remove-nth-node-from-end-of-list/remove-nth-node-from-end-of-list.js
+++ 19-remove-nth-node-from-end-of-list/remove-nth-node-from-end-of-list.js
@@ -6,26 +6,48 @@
  * }
  */
 /**
- * @param {ListNode} head
+ * @param {ListNode ̰} head
  * @param {number} n
  * @return {ListNode}
  */
-var removeNthFromEnd = function(head, n) {
-    let size = 0;
-    let sentinel = new ListNode()
-    sentinel.next = head
-    while(head){
-        size += 1
-        head = head.next
+var removeNthFromEnd = function (head, n) {
+
+    // ******* Approach 1- brute force - two pass approach *******
+
+    // let size = 0;
+    // let sentinel = new ListNode()
+    // sentinel.next = head
+    // while (head) {
+    //     size += 1
+    //     head = head.next
+    // }
+    // let prev = sentinel
+    // let prevPos = size - n
+
+    // for (let i = 0; i < prevPos; i++) {
+    //     prev = prev.next
+    // }
+    // prev.next = prev.next.next
+
+    // return sentinel.next
+
+
+    // ****** Approach- 2: one pass ******
+
+    let sentinel = new ListNode();
+    sentinel.next = head;
+    prev = sentinel;
+    let fast = head
+    for(let i = 0; i<n; i++ ){
+        fast = fast.next
     }
-    let prev = sentinel
-    let prevPos = size-n
-    
-    for(let i=0; i< prevPos; i++){
+    while(fast){
+        fast = fast.next
         prev = prev.next
     }
     prev.next = prev.next.next
 
     return sentinel.next
+
 
 };
