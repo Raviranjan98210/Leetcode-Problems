513c214ac6bfa76b4079ce8f30f656cb845d57ae
RAVI RANJAN KUMAR <32854109+Raviranjan98210@users.noreply.github.com>
2026-06-09 00:48:43 +0530

    Time: 0 ms (100.00%) | Memory: 56.3 MB (88.59%) - LeetSync

--- 21-merge-two-sorted-lists/merge-two-sorted-lists.js
+++ 21-merge-two-sorted-lists/merge-two-sorted-lists.js
@@ -6,33 +6,59 @@
  * }
  */
 /**
- * @param {ListNode} list1
- * @param {ListNode} list2
+ * @param {ListNode} l1
+ * @param {ListNode} l2
  * @return {ListNode}
  */
-var mergeTwoLists = function (list1, list2) {
-    let l3 = new ListNode()
-    l3Head = l3
+var mergeTwoLists = function (l1, l2) {
+    // let l3 = new ListNode()
+    // l3Head = l3
 
-    while (list1 && list2) {
-        if (list1.val < list2.val) {
-            let newNode = new ListNode(list1.val)
-            l3.next = newNode
-            l3 = l3.next
-            list1 = list1.next
+    // while (l1 && l2) {
+    //     if (l1.val < l2.val) {
+    //         let newNode = new ListNode(l1.val)
+    //         l3.next = newNode
+    //         l3 = l3.next
+    //         l1 = l1.next
+    //     } else {
+    //         let newNode = new ListNode(l2.val)
+    //         l3.next = newNode
+    //         l3 = l3.next
+    //         l2 = l2.next
+    //     }
+    // }
+    // if(l1){
+    //     l3.next = l1
+    // }
+    // if(l2){
+    //     l3.next = l2
+    // }
+
+    // return l3Head.next
+
+    // Approach - 2 (no need of l3) 
+
+    let start = new ListNode()
+    let curr = start
+
+    while (l1 && l2) {
+        if (l1.val < l2.val) {
+            curr.next = l1
+            curr = curr.next
+            l1 = l1.next
         } else {
-            let newNode = new ListNode(list2.val)
-            l3.next = newNode
-            l3 = l3.next
-            list2 = list2.next
+            curr.next = l2
+            curr = curr.next
+            l2 = l2.next
         }
     }
-    if(list1){
-        l3.next = list1
+    if(l1){
+        curr.next = l1
     }
-    if(list2){
-        l3.next = list2
+    if(l2){
+        curr.next = l2
     }
 
-    return l3Head.next
+    return start.next
+
 };
