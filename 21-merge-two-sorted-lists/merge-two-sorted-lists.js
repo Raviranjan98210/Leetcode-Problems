9d34e826faf5c8ada3cce993dd1b19b11cb740eb
RAVI RANJAN KUMAR <32854109+Raviranjan98210@users.noreply.github.com>
2026-06-09 00:16:23 +0530

    Time: 1 ms (33.57%) | Memory: 55.2 MB (99.33%) - LeetSync

--- 21-merge-two-sorted-lists/merge-two-sorted-lists.js
+++ 21-merge-two-sorted-lists/merge-two-sorted-lists.js
@@ -10,22 +10,35 @@
  * @param {ListNode} list2
  * @return {ListNode}
  */
-var mergeTwoLists = function(list1, list2) {
-    //Solution:1 -> Using extra space
-    let dummy = new ListNode()
-    let curr =  dummy;
- 
-    while(list1 && list2) {
-        if(list1.val <= list2.val){
-            curr.next = list1
+var mergeTwoLists = function (list1, list2) {
+    let l3 = new ListNode()
+    l3Head = l3
+
+    while (list1 && list2) {
+        if (list1.val < list2.val) {
+            let newNode = new ListNode(list1.val)
+            l3.next = newNode
+            l3 = l3.next
             list1 = list1.next
-        }else {
-            curr.next = list2;
+        } else {
+            let newNode = new ListNode(list2.val)
+            l3.next = newNode
+            l3 = l3.next
             list2 = list2.next
         }
-        curr = curr.next
     }
-    curr.next = list1 || list2
-    return dummy.next;
 
+    while (list1) {
+        let newNode = new ListNode(list1.val)
+        l3.next = newNode
+        l3 = l3.next
+        list1 = list1.next
+    }
+    while (list2) {
+        let newNode = new ListNode(list2.val)
+        l3.next = newNode
+        l3 = l3.next
+        list2 = list2.next
+    }
+    return l3Head.next
 };
