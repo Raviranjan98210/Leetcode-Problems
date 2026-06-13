18357c6eb9173218319125716bd979e9038f463d
RAVI RANJAN KUMAR <32854109+Raviranjan98210@users.noreply.github.com>
2026-06-14 00:49:49 +0530

    Time: 0 ms (100.00%) | Memory: 53 MB (94.64%) - LeetSync

--- 782-jewels-and-stones/jewels-and-stones.js
+++ 782-jewels-and-stones/jewels-and-stones.js
@@ -14,9 +14,9 @@ var numJewelsInStones = function(jewels, stones) {
 
     // Optimal solution
 
-    const j = new Set(jewels)
+    const j = new Set(jewels) // Space complexity: O(1) -> since storing the limites unique character that can be 52 (26 (small)+ 26 (capital) letters), this small number can be ignored 
     for(let i=0; i<stones.length;i++){
-        if(j.has(stones[i])) count++
+        if(j.has(stones[i])) count++ // time complexity: O(1)
     }
     return count
     
