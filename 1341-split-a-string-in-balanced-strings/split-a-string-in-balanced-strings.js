6eff3433caa07a89a5a60ea6f56cc8ed6ed13380
RAVI RANJAN KUMAR <32854109+Raviranjan98210@users.noreply.github.com>
2026-06-16 01:00:36 +0530

    Time: 0 ms (100.00%) | Memory: 53.3 MB (91.66%) - LeetSync

---
+++ 1341-split-a-string-in-balanced-strings/split-a-string-in-balanced-strings.js
@@ -0,0 +1,20 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var balancedStringSplit = function(s) {
+    let count = 0;
+    let balancedCount = 0 
+    for(let i = 0 ; i< s.length; i++){
+        if(s[i] === "R"){
+            ++count
+        }else{
+            --count
+        }
+
+        if(count === 0){
+            ++balancedCount
+        }
+    }
+    return balancedCount
+};
