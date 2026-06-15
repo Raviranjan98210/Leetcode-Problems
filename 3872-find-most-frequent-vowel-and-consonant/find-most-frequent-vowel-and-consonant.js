5a905094a2f6f0c25dd035404e04a208354f97cc
RAVI RANJAN KUMAR <32854109+Raviranjan98210@users.noreply.github.com>
2026-06-16 00:06:07 +0530

    Time: 14 ms (9.96%) | Memory: 57.4 MB (47.27%) - LeetSync

--- 3872-find-most-frequent-vowel-and-consonant/find-most-frequent-vowel-and-consonant.js
+++ 3872-find-most-frequent-vowel-and-consonant/find-most-frequent-vowel-and-consonant.js
@@ -4,20 +4,41 @@
  */
 var maxFreqSum = function(s) {
 
-    let m = new Map()
-    for(let i=0; i< s.length; i++){
-        m.set(s[i],(m.get(s[i])|| 0)+1)
-    }
-    let vf = 0
-    let cf = 0
+    // let m = new Map()
+    // for(let i=0; i< s.length; i++){
+    //     m.set(s[i],(m.get(s[i])|| 0)+1)
+    // }
+    // let vf = 0
+    // let cf = 0
 
-    for(const [ch,count] of m){
-        if(["a","e","i","o","u"].includes(ch)){
-            vf = Math.max(vf,count)
-        }else{
-            cf = Math.max(cf,count)
+    // for(const [ch,count] of m){
+    //     if(["a","e","i","o","u"].includes(ch)){
+    //         vf = Math.max(vf,count)
+    //     }else{
+    //         cf = Math.max(cf,count)
+    //     }
+    // }
+    // return vf+cf
+    
+    const isVowel = (c) => {
+        return c === "a" || c === "e" || c === "i" || c === "o" || c === "u";
+    };
+
+    const mp = {};
+    for (const ch of s) {
+        mp[ch] = (mp[ch] || 0) + 1;
+    }
+
+    let vowel = 0,
+        consonant = 0;
+    for (let ch = "a".charCodeAt(0); ch <= "z".charCodeAt(0); ch++) {
+        const char = String.fromCharCode(ch);
+        const count = mp[char] || 0;
+        if (isVowel(char)) {
+            vowel = Math.max(vowel, count);
+        } else {
+            consonant = Math.max(consonant, count);
         }
     }
-    return vf+cf
-    
+    return vowel + consonant;
 };
