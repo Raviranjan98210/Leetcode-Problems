645688207e5d794deb03f270f1780da551b5ee3e
RAVI RANJAN KUMAR <32854109+Raviranjan98210@users.noreply.github.com>
2026-06-15 23:43:38 +0530

    Time: 5 ms (81.35%) | Memory: 57.1 MB (52.64%) - LeetSync

---
+++ 3872-find-most-frequent-vowel-and-consonant/find-most-frequent-vowel-and-consonant.js
@@ -0,0 +1,23 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var maxFreqSum = function(s) {
+
+    let m = new Map()
+    for(let i=0; i< s.length; i++){
+        m.set(s[i],(m.get(s[i])|| 0)+1)
+    }
+    let vf = 0
+    let cf = 0
+
+    for(const [ch,count] of m){
+        if(["a","e","i","o","u"].includes(ch)){
+            vf = Math.max(vf,count)
+        }else{
+            cf = Math.max(cf,count)
+        }
+    }
+    return vf+cf
+    
+};
