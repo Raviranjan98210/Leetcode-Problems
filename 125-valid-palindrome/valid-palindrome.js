6b572f1e37fa8585f66b15b54482586e15df92e3
RAVI RANJAN KUMAR <32854109+Raviranjan98210@users.noreply.github.com>
2026-06-18 23:49:43 +0530

    Time: 4 ms (76.57%) | Memory: 60.7 MB (24.97%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.js
+++ 125-valid-palindrome/valid-palindrome.js
@@ -2,17 +2,37 @@
  * @param {string} s
  * @return {boolean}
  */
-var isPalindrome = function(s) {
-    s = s.toLowerCase().replace(/[^a-zA-Z0-9]/g, '')
+var isPalindrome = function (s) {
+    // s = s.toLowerCase().replace(/[^a-zA-Z0-9]/g, '')
 
-    let start = 0;
-    let end = s.length-1
-    while(start<=end){
-        if(s[start] !== s[end]){
+    // let start = 0;
+    // let end = s.length-1
+    // while(start<=end){
+    //     if(s[start] !== s[end]){
+    //         return false
+    //     }
+    //     start++
+    //     end--
+    // }
+    // return true
+
+    //Approach -2 
+
+    s = s.toLowerCase()
+    let start = 0
+    let end = s.length - 1
+    const reg = /[a-z0-9]/i
+    while(start < end) {
+        if(!s[start].match(reg)){
+            ++start
+        }else if(!s[end].match(reg)){
+            --end
+        }else if(s[start] === s[end]){
+            ++start
+            --end
+        }else {
             return false
         }
-        start++
-        end--
     }
     return true
 };
