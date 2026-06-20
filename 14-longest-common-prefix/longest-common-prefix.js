05acfcd19483126c070ed7a1e7cca76057165d0d
RAVI RANJAN KUMAR <32854109+Raviranjan98210@users.noreply.github.com>
2026-06-21 00:13:06 +0530

    Time: 2 ms (43.69%) | Memory: 53.7 MB (82.98%) - LeetSync

--- 14-longest-common-prefix/longest-common-prefix.js
+++ 14-longest-common-prefix/longest-common-prefix.js
@@ -4,16 +4,30 @@
  */
 var longestCommonPrefix = function(s) {
     // Take first word, iterate over each character till it is common to all words
-    let fw = s[0]
-    let cp = ""
-    for(let i = 0 ; i < fw.length ; i++){
-        let ch = fw[i]
-        for(let j=1; j<s.length; j++){
-            if( ch !== s[j][i]){
-                return cp
+    // let fw = s[0]
+    // let cp = ""
+    // for(let i = 0 ; i < fw.length ; i++){
+    //     let ch = fw[i]
+    //     for(let j=1; j<s.length; j++){
+    //         if( ch !== s[j][i]){
+    //             return cp
+    //         }
+    //     }
+    //     cp += ch
+    // }
+    // return cp
+
+    // Approach -2 
+
+    let x = 0;
+    while(x < s[0].length){
+        let ch = s[0][x]
+        for(let i = 1; i < s.length; i++){
+            if(s[i][x] !== ch || x===s[i].length){
+                return s[0].substring(0,x)
             }
         }
-        cp += ch
+        ++x
     }
-    return cp
+    return s[0]
 };
