344c73e6f6ee1d8d1c4b91ae00ae22fa4f9f87a8
RAVI RANJAN KUMAR <32854109+Raviranjan98210@users.noreply.github.com>
2026-06-23 02:07:57 +0530

    Time: 7 ms (50.31%) | Memory: 55.3 MB (44.40%) - LeetSync

--- 205-isomorphic-strings/isomorphic-strings.js
+++ 205-isomorphic-strings/isomorphic-strings.js
@@ -4,17 +4,35 @@
  * @return {boolean}
  */
 var isIsomorphic = function(s, t) {
-    const m = new Map()
-    if(s.length !== t.length) return false
-    for(let i= 0; i<s.length; i++){
-        if(m.has(s[i])) continue
-        if([...m.values()].includes(t[i])) return false
-        m.set(s[i],t[i])
-    }
+    // const m = new Map()
+    // if(s.length !== t.length) return false
+    // for(let i= 0; i<s.length; i++){
+    //     if(m.has(s[i])) continue
+    //     if([...m.values()].includes(t[i])) return false
+    //     m.set(s[i],t[i])
+    // }
 
-    let str = ""
+    // let str = ""
+    // for(let i=0; i<s.length; i++){
+    //     str += m.get(s[i])
+    // }
+    // return t===str
+
+    // approach-2
+
+    let s2t = new Map()
+    let t2s = new Map()
     for(let i=0; i<s.length; i++){
-        str += m.get(s[i])
+        if(!s2t.has(s[i]) && !t2s.has(t[i])){
+            s2t.set(s[i],t[i])
+            t2s.set(t[i],s[i])
+        }else if(s2t.get(s[i]) !== t[i] || t2s.get(t[i]) !== s[i]) return false
+
     }
-    return t===str
+    return true
+
+//foo
+//bar
+//far
+// boo
 };
