22bececc4db48b954d98f89830d0c9d8bd9014aa
RAVI RANJAN KUMAR <32854109+Raviranjan98210@users.noreply.github.com>
2026-07-02 22:21:55 +0530

    Time: 6 ms (58.54%) | Memory: 55 MB (98.65%) - LeetSync

---
+++ 150-evaluate-reverse-polish-notation/evaluate-reverse-polish-notation.js
@@ -0,0 +1,24 @@
+/**
+ * @param {string[]} tokens
+ * @return {number}
+ */
+var evalRPN = function(tokens) {
+     let stack = [];
+    const map = {
+        "+": (a,b) => (b+a),
+        "*": (a,b) => (b*a),
+        "-": (a,b) => (b-a),
+        "/": (a,b) => Math.trunc(b/a),
+    };
+        for(let i=0; i < tokens.length; i++){
+            if(map[tokens[i]]) {
+                let a = stack.pop();
+                let b = stack.pop();
+                let ans = map[tokens[i]](+a,+b);
+                stack.push(ans);
+            } else {
+                stack.push(tokens[i]) 
+            }
+        }
+    return Number(stack.pop());
+};
