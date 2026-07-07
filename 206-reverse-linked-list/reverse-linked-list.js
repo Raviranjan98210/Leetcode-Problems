b7592da27251905786b566ee3107492424504070
RAVI RANJAN KUMAR <32854109+Raviranjan98210@users.noreply.github.com>
2026-07-07 23:42:49 +0530

    Time: 0 ms (100.00%) | Memory: 58 MB (33.25%) - LeetSync

--- 206-reverse-linked-list/reverse-linked-list.js
+++ 206-reverse-linked-list/reverse-linked-list.js
@@ -10,21 +10,14 @@
  * @return {ListNode}
  */
 var reverseList = function (head) {
-    //  1   3  4 -> 5
-                 //curr 
-
-    //  4->3-> 2-> 1 -> null
-    // prev
     let prev = null
     let curr = head
-    
     while (curr) {
         let next = curr.next
         curr.next = prev
         prev = curr
         curr = next
     }
-
     return prev
 
 };
