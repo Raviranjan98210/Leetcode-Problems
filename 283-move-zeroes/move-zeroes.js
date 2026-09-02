94252b8ea353a62dff0df719ae39c66c01ce33ce
RAVI RANJAN KUMAR <32854109+Raviranjan98210@users.noreply.github.com>
2026-09-03 00:27:49 +0530

    Time: 1 ms (77.13%) | Memory: 58.6 MB (93.62%) - LeetSync

--- 283-move-zeroes/move-zeroes.js
+++ 283-move-zeroes/move-zeroes.js
@@ -2,49 +2,22 @@
  * @param {number[]} nums
  * @return {void} Do not return anything, modify nums in-place instead.
  */
-var moveZeroes = function(nums) {
-    // Brute Force
-    // const nonZero = [];
-    // for(let i = 0; i<nums.length;i++){
-    //     if(nums[i] !== 0) {
-    //         nonZero.push(nums[i])
-    //     }
-    // }
-    // for(let i=0; i < nonZero.length; i++){
-    //     nums[i] = nonZero[i]
-    // }
-    // for(let i = nonZero.length; i<nums.length; i++){
-    //     nums[i] = 0
-    // }
+var moveZeroes = function (nums) {
+    // will keep two pointer 
+    //first that will hold the position where non-zero element should be placed
+    // 2nd: iterator which keep iterating the array
 
-//     let p1=0;
-//     let p2=1;
+    let nzPos = 0
 
-//    while(p2<nums.length){
-//         if(nums.length === 1)  return nums;
-//         if(nums[p1] === 0 && nums[p2]!==0){
-//             [nums[p1],nums[p2]] = [nums[p2],nums[p1]]
-//             p1++;
-//             p2++;
-//         } else if(nums[p1]!== 0) {
-//             p1++;
-//             p2++;
-//         }else {
-//             p2++
-//         }
-//     }
-
-//Aproach: 3
-
-let p = 0;
-for(let i=0; i<nums.length; i++){
-    if(nums[i] !== 0){
-        nums[p] = nums[i];
-        p++;
+    for (let i = 0; i < nums.length; i++) {
+        if (nums[i] !== 0) {
+            nums[nzPos] = nums[i]
+            nzPos++
+        }
     }
-}
-for(let i=p; i<nums.length;i++) {
-    nums[i] = 0
-}
+    for (let j = nzPos; j < nums.length; j++) {
+        nums[j] = 0
+    }
 
 };
+
