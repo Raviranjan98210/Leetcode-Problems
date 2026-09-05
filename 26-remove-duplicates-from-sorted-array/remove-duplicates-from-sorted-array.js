2c8f3d652be51f523b7bc36d88f0c7e028380a2d
RAVI RANJAN KUMAR <32854109+Raviranjan98210@users.noreply.github.com>
2026-09-06 00:11:20 +0530

    Time: 1 ms (49.29%) | Memory: 58.1 MB (43.58%) - LeetSync

--- 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.js
+++ 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.js
@@ -3,33 +3,16 @@
  * @return {number}
  */
 var removeDuplicates = function(nums) {
-    // const l = nums.length;
-    // let j = 1;
-    // let i = 0; 
-    // const arr = []
-    // while(i < l ) {
-    //     if(nums[i]=== nums[j]) {
-    //         j++;
-    //     }else {
-    //       arr.push(nums[i])
-    //       i=j;
-    //       j++;
-    //     }
-    // }
+    if(nums.length === 1) return
+    let uniq = 0
+    let uniqCount = 1
 
-    // for(let i=0; i<arr.length ; i++) {
-    //         nums[i]= arr[i]
-    // }
-    // nums.splice(arr.length)
-
-    ///Approach 2 (optimal)
-
-    let u = 0;
-    for(let i=0; i < nums.length; i++){
-        if(nums[i]>nums[u]) {
-            u = u+1;
-            nums[u] = nums[i]
+    for(let i = 1; i<nums.length; i++){
+        if(nums[i] !== nums[i-1]){
+            nums[uniq + 1] = nums[i]
+            uniq++
+            uniqCount += 1
         }
-    }
-    return u+1;
+    }   
+    return uniqCount
 };
