9d82677517910f22ad77f2463ecce3f036781de5
RAVI RANJAN KUMAR <32854109+Raviranjan98210@users.noreply.github.com>
2026-09-09 13:10:08 +0530

    Time: 4 ms (19.35%) | Memory: 56.7 MB (16.13%) - LeetSync

---
+++ 4245-count-commas-in-range/count-commas-in-range.js
@@ -0,0 +1,12 @@
+/**
+ * @param {number} n
+ * @return {number}
+ */
+var countCommas = function (n) {
+    if (n < 1000) return 0
+    let count = 0
+    for (let i = 1000; i <= n; i++) {
+        count++
+    }
+    return count
+};
